9d46b674624c79ee2d3427922d5c2e870db91b5c
sbx_user1051 <sbx_user1051@169.254.14.73>
2025-07-26 16:27:38 +0000

    Sat Jul 26 16:27:38 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -24,25 +24,26 @@ class CheckoutSolution:
         for i in items_purchased:
             # i = A
             quantity = items_purchased[i] # 35
-            offer_unit = 0
+            offer_unit = 1
             offer_price = 0
             price = 0
             if i in price_table:
                 if "special_offers" in price_table[i]:
-                    offer_unit = price_table[i]["special_offers"]["unit"] # 3
+                    offer_unit = price_table[i]["special_offers"]["units"] # 3
                     offer_price = price_table[i]["special_offers"]["price"] # 130
             
                 price = price_table[i]["price"] # 50
 
-            if offer_unit != 0:
-                quotient = quantity//offer_unit
-                remainder = quantity % offer_unit
+            
+            quotient = quantity//offer_unit
+            remainder = quantity % offer_unit
 
-                item_total_cost = (quotient * offer_price) + (remainder * price)
-                
-                basket_total = basket_total + item_total_cost
+            item_total_cost = (quotient * offer_price) + (remainder * price)
+            
+            basket_total = basket_total + item_total_cost
                 
         return basket_total
+
 
 
 
