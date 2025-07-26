d0384a0dea2378f89c5dcb6faf418cc133f9fafb
sbx_user1051 <sbx_user1051@169.254.14.73>
2025-07-26 19:06:51 +0000

    Sat Jul 26 19:06:51 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,7 +5,7 @@ class CheckoutSolution:
     def checkout(self, skus):
         basket_total = 0
         price_table = {"A": {"price" : 50, "special_offers" : [{"units" : 5, "price" : 200}, {"units" : 3, "price" : 130}]},
-                "B": {"price" : 30, "special_offers" : {"units" : 2, "price" : 45}},
+                "B": {"price" : 30, "special_offers" : [{"units" : 2, "price" : 45}]},
                 "C": {"price" : 20},
                 "D": {"price" : 15},
                 "E": {"price" : 40,  "multi_offer" : {"units" : 2, "free_product" : "B", "free_quantity" : 1}}}
@@ -32,7 +32,6 @@ class CheckoutSolution:
                 if "special_offers" in price_table[i]:
                     j = 0
                     while j >= 0:
-                        print(price_table[i]["special_offers"][j]["price"])
                         offer_unit = price_table[i]["special_offers"][j]["units"] # 5
                         offer_price = price_table[i]["special_offers"][j]["price"] # 200
                 
@@ -54,6 +53,7 @@ class CheckoutSolution:
             basket_total = basket_total + item_total_cost
                 
         return basket_total
+
 
 
 
