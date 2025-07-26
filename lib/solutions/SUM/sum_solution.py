8a40e6dcf9a54af3675537cfef99b23cafa99af9
sbx_user1051 <sbx_user1051@169.254.14.73>
2025-07-26 14:41:24 +0000

    Sat Jul 26 14:41:24 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -11,9 +11,11 @@ class SumSolution:
             Returns:
                 int: Sum of a and b.
                 
-            """
+        """
+        
         if (isinstance(a,int) and a > 0 and a < 100) and (isinstance(b,int) and b > 0 and b < 100):
             return a + b
         else:
             raise ValueError("All imputs must be and integer between 0 and 100")
+
 
