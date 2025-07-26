675ef9c0d92a748642dbe0b4fd7a8e0a39f6ca52
sbx_user1051 <sbx_user1051@169.254.14.73>
2025-07-26 14:38:24 +0000

    Sat Jul 26 14:38:24 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -2,4 +2,18 @@
 class SumSolution:
     
     def compute(self, x, y):
-        raise NotImplementedError()
+        """Return the result of sum a and b
+            
+            Args:
+                a (int): Positive Integer between 0 and 100
+                b (int): Positive Integer between 0 and 100.
+            
+            Returns:
+                int: Sum of a and b.
+                
+            """
+        if (isinstance(a,int) and a > 0 and a < 100) and (isinstance(b,int) and b > 0 and b < 100):
+            return a + b
+        else:
+            raise ValueError("All imputs must be and integer between 0 and 100")
+
